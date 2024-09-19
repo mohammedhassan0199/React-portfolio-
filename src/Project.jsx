5e662204321319c91f78ea49b533145252f79955
Mohammed Hassan <mohammedhassan0199@gmail.com>
2024-09-19 20:30:09 +0530

    error fixed my portfolio

--- src/Project.jsx
+++ src/Project.jsx
@@ -7,10 +7,14 @@ function Project() {
         AOS.init();
       }, [])
   return (
-    <section className="px-[7%] py-14 flex flex-col justify-start items-center" id='project'>
+    <section className="px-[7%] py-14 flex flex-col items-center" id='project'>
 
-    <div>
-        <h2 className="text-6xl font-bold my-6  lg:text-5xl md:text-4xl sm:text-2xl">Projects</h2>
+    <div >
+        <h2 className="text-6xl font-bold my-6  lg:text-5xl md:text-4xl sm:text-2xl text-center">Projects</h2>
+       <div className='grid grid-cols-3 gap-6'>
+       <a href="https://github.com/mohammedhassan0199/React-portfolio-" target='-main'><img src="portfolio.png" className='w-[200px]' alt="" /></a>
+       
+       </div>
        
     </div>
     
