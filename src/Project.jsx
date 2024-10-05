7e5bc0011f97293ae5980337abe44a9a470762c9
Mohammed Hassan <mohammedhassan0199@gmail.com>
2024-10-05 16:32:10 +0530

    some thing added

--- src/Project.jsx
+++ src/Project.jsx
@@ -13,9 +13,9 @@ function Project() {
         <h2 className="text-6xl font-bold my-6  lg:text-5xl md:text-4xl sm:text-2xl text-center">Projects</h2>
        <div className='grid grid-cols-3 gap-6 text-[#006315] font-semibold text-center'>
        <a href="https://github.com/mohammedhassan0199/React-portfolio-" target='-main'><img src="portfolio.png" className='w-[200px]' alt="" />Portfolio</a>
-       <a href="https://github.com/mohammedhassan0199/Netflix-clone"><img className='w-[200px]' src="netflix.png" alt="" />Netflix Clone</a>
+       <a href="https://github.com/mohammedhassan0199/Netflix-clone" target='-main'><img className='w-[200px]' src="netflix.png" alt="" />Netflix Clone</a>
 
-       <a href="https://github.com/mohammedhassan0199/CryptoCurrency-api"><img className='w-[200px]' src="crypto.png" alt="" />Crypto Currency Api Project</a>
+       <a href="https://github.com/mohammedhassan0199/CryptoCurrency-api" target='-main'><img className='w-[200px]' src="crypto.png" alt="" />Crypto Currency Api Project</a>
        </div>
        
     </div>
