905b6141aa0bec8d8149d26a1dd3f6a47c94c1d5
Mohammed Hassan <mohammedhassan0199@gmail.com>
2024-10-05 16:29:26 +0530

    add projects in portfolio

--- src/Project.jsx
+++ src/Project.jsx
@@ -9,11 +9,13 @@ function Project() {
   return (
     <section className="px-[7%] py-14 flex flex-col items-center" id='project'>
 
-    <div >
+    <div className=''>
         <h2 className="text-6xl font-bold my-6  lg:text-5xl md:text-4xl sm:text-2xl text-center">Projects</h2>
-       <div className='grid grid-cols-3 gap-6'>
-       <a href="https://github.com/mohammedhassan0199/React-portfolio-" target='-main'><img src="portfolio.png" className='w-[200px]' alt="" /></a>
-       
+       <div className='grid grid-cols-3 gap-6 text-[#006315] font-semibold text-center'>
+       <a href="https://github.com/mohammedhassan0199/React-portfolio-" target='-main'><img src="portfolio.png" className='w-[200px]' alt="" />Portfolio</a>
+       <a href="https://github.com/mohammedhassan0199/Netflix-clone"><img className='w-[200px]' src="netflix.png" alt="" />Netflix Clone</a>
+
+       <a href="https://github.com/mohammedhassan0199/CryptoCurrency-api"><img className='w-[200px]' src="crypto.png" alt="" />Crypto Currency Api Project</a>
        </div>
        
     </div>
