af34fe5bbb49364d3bce6c5999c43fc711a8ef48
Mohammed Hassan <mohammedhassan0199@gmail.com>
2024-10-08 17:49:13 +0530

    add whatsapp clone project

--- src/Project.jsx
+++ src/Project.jsx
@@ -11,11 +11,14 @@ function Project() {
 
     <div className=''>
         <h2 className="text-6xl font-bold my-6  lg:text-5xl md:text-4xl sm:text-2xl text-center">Projects</h2>
-       <div className='grid grid-cols-3 gap-6 text-[#006315] font-semibold text-center'>
+
+       <div className='grid grid-cols-3 gap-6 text-[#006315] font-semibold text-center sm:grid-cols-2'>
        <a href="https://github.com/mohammedhassan0199/React-portfolio-" target='-main'><img src="portfolio.png" className='w-[200px]' alt="" />Portfolio</a>
        <a href="https://github.com/mohammedhassan0199/Netflix-clone" target='-main'><img className='w-[200px]' src="netflix.png" alt="" />Netflix Clone</a>
 
        <a href="https://github.com/mohammedhassan0199/CryptoCurrency-api" target='-main'><img className='w-[200px]' src="crypto.png" alt="" />Crypto Currency Api Project</a>
+
+       <a href="https://github.com/mohammedhassan0199/whats-clone" target='-main'><img className='w-[200px]' src="whatsapp.png" alt="" />Whatsapp Clone</a>
        </div>
        
     </div>
